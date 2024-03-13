0fd97e52278e5387ee2624e8ebff05c7a3ae3fa8
AnshSonone <118267956+AnshSonone@users.noreply.github.com>
2024-03-13 17:12:36 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -145,9 +145,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
